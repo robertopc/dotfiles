7e13090c39a4c3f699822f20600d03634882e6ed
robertopc <robertopc.prog@gmail.com>
2018-07-13 16:48:04 -0300

    Added vundle.vim to dotfiles

--- create-links.sh
+++ create-links.sh
@@ -9,6 +9,10 @@ ln .config/terminator/config ~/.config/terminator/config
 mv ~/.config/openbox/lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml.orig
 ln .config/openbox/lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml
 
+mkdir ~/.vim
+mkdir ~/.vim/bundle/
+ln -s ~/dotfiles/.vim/bundle/Vundle.vim ~/.vim/bundle/
+
 # link all dotfiles
 find . -type f -name '.*' -exec ln {} ~ \;
 
